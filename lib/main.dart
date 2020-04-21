a1df27944d5a850a7364f9f656d51b3f3a5dbbfe
ranjeet <ranjeet.floyd@gmail.com>
2020-04-21 14:53:37 +0530

    use random dicee no using Dart Random generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = getRandomDiceNo();
+  int rightDiceNo = getRandomDiceNo();
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,22 +33,32 @@ class DicePage extends StatelessWidget {
             flex: 1,
             child: FlatButton(
               onPressed: () {
+                setState(() {
+                  leftDiceNo = getRandomDiceNo();
+                });
                 print("Letft Button Pressed !!");
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNo.png'),
             ),
           ),
           Expanded(
             flex: 1,
             child: FlatButton(
               onPressed: () {
+                setState(() {
+                  rightDiceNo = getRandomDiceNo();
+                });
                 print("Right Button Pressed !!");
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNo.png'),
             ),
           ),
         ],
       ),
     );
   }
+}
+
+int getRandomDiceNo() {
+  return Random().nextInt(6) + 1;
 }
